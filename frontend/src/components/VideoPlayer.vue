28ff3529995171bc7547bcad5b9678ba900b91f7
Austin Tanksley <58612782+austin-tanksley@users.noreply.github.com>
2025-06-02 14:23:44 -0500

    Update VideoPlayer.vue

--- frontend/src/components/VideoPlayer.vue
+++ frontend/src/components/VideoPlayer.vue
@@ -7,7 +7,7 @@
     title: ""
   })
 
-  const url = 'http://cueclub.onrender.com/api/v1/mix'
+  const url = 'https://cueclub.onrender.com/api/v1/mix'
   let embedURL = ref("")
   const c = ref(null)
 
